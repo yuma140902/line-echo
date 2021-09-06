97cee72416ce4d6c3a957eff78ecafbbe6fc7b4f
yuma140902 <yuma140902@gmail.com>
2021-09-06 09:59:43 +0900

    Revert "official"
    This reverts commit 0b141c9a22b9fcc5d63fc02d050c85629034f8e9.

--- index.js
+++ index.js
@@ -1,64 +1,56 @@
-const https = require("https")
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 3000
-const TOKEN = process.env.LINE_ACCESS_TOKEN
+const https = require('https')
+const express = require('express')
 
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-app.get("/", (req, res) => {
-  res.sendStatus(200)
-})
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/webhook", function(req, res) {
-  res.send("HTTP POST request sent to the webhook URL!")
-  // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
-  if (req.body.events[0].type === "message") {
-    const dataString = JSON.stringify({
+app.get('/', (req, res) => res.sendStatus(200));
+
+app.post('/webhook', (req, res) => {
+  res.send('HTTP POST request sent to the webhook URL!');
+
+  // ユーザーがボットにメッセージを送った場合
+  if (req.body.events[0].type === 'message') {
+    const body = JSON.stringify({
       replyToken: req.body.events[0].replyToken,
       messages: [
         {
-          "type": "text",
-          "text": "Hello, user"
+          'type': 'text',
+          'text': 'Hello, user'
         },
         {
-          "type": "text",
-          "text": "May I help you?"
+          'type': 'text',
+          'text': 'May I help you?'
         }
       ]
-    })
+    });
 
     const headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer " + TOKEN
-    }
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer' + TOKEN
+    };
 
     const webhookOptions = {
-      "hostname": "api.line.me",
-      "path": "/v2/bot/message/reply",
-      "method": "POST",
-      "headers": headers,
-      "body": dataString
-    }
+      'hostname': 'api.line.me',
+      'path': '/v2/bot/message/reply',
+      'method': 'POST',
+      'headers': headers,
+      'body': body
+    };
 
-    const request = https.request(webhookOptions, (res) => {
-      res.on("data", (d) => {
-        process.stdout.write(d)
-      })
-    })
+    const request = https.request(webhookOptions, res => {
+      res.on('data', data => process.stdout.write(d));
+    });
 
-    request.on("error", (err) => {
-      console.error(err)
-    })
+    request.on('error', err => console.error(err));
 
-    request.write(dataString)
-    request.end()
+    request.write(body);
+    request.end();
   }
-})
+});
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+app.listen(PORT, () => console.log(`Listening on :${PORT}`));
