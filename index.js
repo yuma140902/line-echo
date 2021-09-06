a8d1719e746e7f684147fdc00fab604344b2a3ee
yuma140902 <yuma140902@gmail.com>
2021-09-06 22:49:44 +0900

    simple

--- index.js
+++ index.js
@@ -4,73 +4,15 @@ const line = require('@line/bot-sdk')
 
 const PORT = process.env.PORT || 3000;
 
-const config = {
-  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
-};
-
-const dic = {
-  'a': ['apple', 'ant', 'amazon'],
-  'b': ['banana'],
-  'c': ['cacao'],
-  'd': ['domain'],
-  'e': ['e-mail'],
-  'f': ['family'],
-  'g': ['go'],
-  'h': ['hey'],
-  'i': ['internet'],
-  'j': ['Japan'],
-  'k': ['kid'],
-  'l': ['lily'],
-  'm': ['mom'],
-  'n': ['no'],
-  'o': ['occur'],
-  'p': ['piano'],
-  'q': ['quantum'],
-  'r': ['random'],
-  's': ['step'],
-  't': ['tech'],
-  'u': ['ultra'],
-  'v': ['vector'],
-  'w': ['win'],
-  'x': ['x-server'],
-  'y': ['yes'],
-  'z': ['zoom']
-}
-
-// 半開区間 [begin, end)
-function rangedRandom(begin, end) {
-  return Math.floor(Math.random() * (end - begin)) + begin;
-}
-
-function selectRandom(array) {
-  return array[rangedRandom(0, array.length)];
-}
 
 const app = express();
 
-app.get('/', (req, res) => res.sendStatus(200));
-
-app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then(result => res.json(result));
-});
-
-const client = new line.Client(config);
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
+app.get('/', (req, res) => {
+  const name = req.query.name;
+  if (name) {
+    console.log(name);
   }
-
-  const word = event.message.text;
-  const lastletter = word.slice(-1).toLowerCase();
-  const replyWord = selectRandom(dic[lastletter]);
-
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: replyWord ?? 'English word please'
-  });
-}
+  res.send(`Hello ${name ?? 'Unknown'}`);
+});
 
 app.listen(PORT, () => console.log(`Listening on :${PORT}`));
