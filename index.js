10c3d7358f243e603c41e8d10eddee2918bccb8b
yuma140902 <yuma140902@gmail.com>
2021-09-06 19:09:22 +0900

    しりとり

--- index.js
+++ index.js
@@ -9,6 +9,35 @@ const config = {
   channelSecret: process.env.CHANNEL_SECRET
 };
 
+const dic = {
+  'a': 'apple',
+  'b': 'banana',
+  'c': 'cacao',
+  'd': 'domain',
+  'e': 'e-mail',
+  'f': 'family',
+  'g': 'go',
+  'h': 'hey',
+  'i': 'internet',
+  'j': 'Japan',
+  'k': 'kid',
+  'l': 'lily',
+  'm': 'mom',
+  'n': 'no',
+  'o': 'occur',
+  'p': 'piano',
+  'q': 'quantum',
+  'r': 'random',
+  's': 'step',
+  't': 'tech',
+  'u': 'ultra',
+  'v': 'vector',
+  'w': 'win',
+  'x': 'x-server',
+  'y': 'yes',
+  'z': 'zoom'
+}
+
 const app = express();
 
 app.get('/', (req, res) => res.sendStatus(200));
@@ -25,9 +54,13 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
+  const word = event.message.text;
+  const lastletter = word.slice(-1).toLowerCase();
+  const replyWord = dic[lastletter];
+
   return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: event.message.text
+    text: replyWord ?? 'English word please'
   });
 }
 
