a1f97f41bc3fe2599facafc468df1657352bc006
yuma140902 <yuma140902@gmail.com>
2021-09-21 17:55:12 +0900

    db

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: { rejectUnauthorized: false }
 });
 
 const updateUserLastKana = (userId, lastKana) => {
